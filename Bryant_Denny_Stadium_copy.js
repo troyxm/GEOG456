2d7e6943eb3e4d9f817af3908c5ee81d48d9dfa8
Troy <81226012+troyxm@users.noreply.github.com>
2026-01-22 13:04:25 -0500

    HW2 uploaded

---
+++ Bryant_Denny_Stadium_copy.js
@@ -0,0 +1,7 @@
+var stadium = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "descrip": "Bryant-Denny Stadium" }, "geometry": { "type": "Point", "coordinates": [ -87.550461, 33.207749 ] } }
+]
+}
